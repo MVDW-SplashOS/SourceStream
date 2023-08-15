ab21cd8201651ce985fe26dc88a1d0accc36b314
MVDW <maxvdwolf@gmail.com>
2023-08-16 00:36:12 +0200

    Updated help page

--- SourceStream/command/help/help.py
+++ SourceStream/command/help/help.py
@@ -5,7 +5,7 @@ import sys
 def print_help():
     logger.log.print("SourceStream Manual", "\n\n");
 
-    logger.log.print("Usage: 'python3 " + sys.argv[0] + " --<argument 1> --[argument 2]'");
+    logger.log.print("Usage: 'python3 " + sys.argv[0] + " <argument 1> [argument 2]'");
     logger.log.print("<> = required argument")
     logger.log.print("[] = optional argument", "\n\n")
 
@@ -22,9 +22,13 @@ def print_help():
 
     logger.log.print("Available arguments:")
     help_available_args = [
-        "help                      :    Prints out the manual.", ["h", "man", "manual"],
-        "list-packages             :    List out all packages required by the edition.", ["list"],
-        "package:<name>:[version]  :    Select a specific package to pack.", ["p:<name>:[ver]"],
+        "help                                             :  Prints out the manual.", ["h", "man", "manual"],
+        "package:<name>:[version]                         :  Select a specific package to pack.", ["p:<name>:[ver]"],
+        "package-list:[package]:[version]                 :  List out all packages required by the edition.", ["list"],
+        "upstream-add:<name>:[url]:[version]:[checksum]   :  Adding new source to the upstream-sources.yml(will lock yaml file)",
+        "upstream-modify:<name/url>:<new value>           :  Modify existing source(will lock yaml file)",
+        "upstream-checksum:<name>:<version>:<checksum>    :  Update or add checksum for spesific version(will lock yaml file)",
+        "upstream-remove:<name>                           :  Removes existing source(will lock yaml file)",
 
     ]
 

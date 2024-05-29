31b929e0d4f78581948c6c1473d049fc3d1b9301
MVDW <maxvdwolf@gmail.com>
2024-05-29 22:04:42 +0200

    Updated help page

--- SourceStream/command/help/help.py
+++ SourceStream/command/help/help.py
@@ -26,10 +26,11 @@ def print_help():
         #"serv                                             :  Running SourceStream as a service.",
         "package:<name>:[version]                         :  Select a specific package to pack.", ["p:<name>:[ver]"],
         "package-list:[package]:[version]                 :  List out all packages required by the edition.", ["list"],
-        "upstream-add:<name>:[url]:[version]:[checksum]   :  Adding new source to the upstream-sources.yml(will lock yaml file)",
-        "upstream-modify:<name/url>:<new value>           :  Modify existing source(will lock yaml file)",
-        "upstream-checksum:<name>:<version>:<checksum>    :  Update or add checksum for spesific version(will lock yaml file)",
-        "upstream-remove:<name>                           :  Removes existing source(will lock yaml file)",
+        "upstream-add:<name>:[url]:[version]:[checksum]   :  Adding new source to the upstream-sources.yml (will lock yaml file)",
+        "upstream-modify:<name/url>:<new value>           :  Modify existing source (will lock yaml file)",
+        "upstream-checksum:<name>:<version>:<checksum>    :  Update or add checksum for spesific version (will lock yaml file)",
+        "upstream-remove:<name>                           :  Removes existing source (will lock yaml file)",
+        "upstream-upgrade:<edition>:[package]             :  Upgrade selected edition configuration file with latest version from LFS documentation."
 
     ]
 

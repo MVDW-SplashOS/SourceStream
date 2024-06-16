45e1e559c2b1c62f25baab081692a5276bcce7ca
BruddaCode <fabio.wolthuis@gmail.com>
2024-06-16 16:08:39 +0200

    speedup downloading

--- SourceStream/file/download.py
+++ SourceStream/file/download.py
@@ -2,27 +2,54 @@ from .. import SourceStream
 
 from ..vendor.SplashPyUtils import logger
 
-import requests
+import asyncio
+import aiohttp
+import time
 import os
 
-def download_tool(package):
-    package_name = package["package"]
-    package_version = package["version"]
-    package_version_split = package["version"].split(".")
+async def download_tool(packages):
+    
+    package_list = []
+    
+    for package in packages:
+        package_name = package["package"]
+        package_version = package["version"]
+        package_version_split = package["version"].split(".")
 
-    package_url = SourceStream.YAML_SOURCES[package_name]["url"].replace("{VERSION}", package_version)
-    package_url = package_url.replace("{VERSION_SEPARATE_UNDERSCORE}", package_version.replace(".", "_"))
+        package_url = SourceStream.YAML_SOURCES[package_name]["url"].replace("{VERSION}", package_version)
+        package_url = package_url.replace("{VERSION_SEPARATE_UNDERSCORE}", package_version.replace(".", "_"))
 
-    if(len(package_version_split) >= 1): package_url = package_url.replace("{VERSION_MAJOR}", package_version_split[0])
-    if(len(package_version_split) >= 2): package_url = package_url.replace("{VERSION_MINOR}", package_version_split[1])
-    if(len(package_version_split) >= 3): package_url = package_url.replace("{VERSION_REVISION}", package_version_split[2])
-    if(len(package_version_split) >= 4): package_url = package_url.replace("{VERSION_BUILD}", package_version_split[3])
+        if(len(package_version_split) >= 1): package_url = package_url.replace("{VERSION_MAJOR}", package_version_split[0])
+        if(len(package_version_split) >= 2): package_url = package_url.replace("{VERSION_MINOR}", package_version_split[1])
+        if(len(package_version_split) >= 3): package_url = package_url.replace("{VERSION_REVISION}", package_version_split[2])
+        if(len(package_version_split) >= 4): package_url = package_url.replace("{VERSION_BUILD}", package_version_split[3])
 
-    package_filename = os.path.basename(package_url)
-    package_path = os.path.join(SourceStream.DIR_INPUT, package_filename)
-
-    if not os.path.exists(package_path):
-        r = requests.get(package_url)
-        open(package_path, 'wb').write(r.content)
-        logger.log.ok(f"Downloaded package \x1b[1;37m{package_name}({package_version})\x1b[0m Successfully.")
+        package_filename = os.path.basename(package_url)
+        package_path = os.path.join(SourceStream.DIR_INPUT, package_filename)
+        
+        if not os.path.exists(package_path):
+            package_list.append({"name": package_name, "version": package_version, "url": package_url, "path": package_path})
+        else:
+            logger.log.ok(f"Package \x1b[1;37m{package_name}({package_version})\x1b[0m already downloaded.")
+    
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for package in package_list:
+            tasks.append(download_package(session, package))
+        
+        await asyncio.gather(*tasks)
+        
+async def download_package(session, package):
+    async with session.get(package["url"]) as response:
+        if response.status == 200:
+            with open(package["path"], 'wb') as f_handle:
+                while True:
+                    chunk = await response.content.read(1024)
+                    if not chunk:
+                        break
+                    f_handle.write(chunk)
+            logger.log.ok(f"Downloaded package \x1b[1;37m{package['name']}({package['version']})\x1b[0m Successfully.")
+        else:
+            logger.log.fail(f"Failed to download package \x1b[1;37m{package['name']}({package['version']})\x1b[0m.")
+            logger.log.fail(f"Status code: {response.status}")
 
